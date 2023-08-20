80454dfb1745b50828c4f76614a0563105309ce9
wolfiee42 <saifalislam2022@gmail.com>
2023-08-20 19:28:14 +0600

    total section done

--- script/utilities.js
+++ script/utilities.js
@@ -1,14 +1,14 @@
-let sum = 0;
+let total = 0;
 function namePrice(data) {
     const name = data.parentNode.childNodes[1].childNodes[5].innerText;
     const price = data.parentNode.childNodes[1].childNodes[7].innerText;
     const checkout = document.getElementById('checkOutSection').parentNode.childNodes[3].childNodes[1];
-
-
+    
     const p = document.createElement('p');
     p.innerText = name + ': ' + price;
     checkout.appendChild(p);
-
+    
     const priceValue = parseFloat(price);
-    sum += priceValue;
+    total = parseFloat(total) + parseFloat(priceValue);
+    const totalSec = document.getElementById('totalPrice').innerText = total;
 }
