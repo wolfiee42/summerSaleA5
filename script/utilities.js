ab493c8c872a7132d05f89920e814dab2e55bb01
wolfiee42 <saifalislam2022@gmail.com>
2023-08-20 23:28:03 +0600

    function part added

--- script/utilities.js
+++ script/utilities.js
@@ -21,22 +21,23 @@ function namePrice(data) {
     const priceValue = parseFloat(price);
     total = parseFloat(total) + parseFloat(priceValue);
 
-    document.getElementById('totalPrice').innerText = total;
+    const ammount = document.getElementById('totalPrice').innerText = total;
 
 
-    // return priceValue;
+    if (ammount > 200) {
+        document.getElementById('coupon-Code').addEventListener('keyup', function (event) {
+            const input = event.target.value;
+            const btn = document.getElementById('apply-btn');
+
+            if (input === "aa") {
+                btn.removeAttribute('disabled');
+            } else {
+                btn.setAttribute("disabled", true)
+            }
+        })
+    }
 }
 
-document.getElementById('coupon-Code').addEventListener('keyup', function (event) {
-    const input = event.target.value;
-    const btn = document.getElementById('apply-btn');
-
-    if (input === "aa") {
-        btn.removeAttribute('disabled');
-    } else {
-        btn.setAttribute("disabled", true)
-    }
-})
 
 
 
@@ -44,6 +45,3 @@ document.getElementById('coupon-Code').addEventListener('keyup', function (event
 
 
 
-// const totalAmmount = document.getElementById('total').innerText;
-//     totalAmmount = parseFloat(priceValue) - parseFloat(discount);
-//     console.log(totalAmmount);
