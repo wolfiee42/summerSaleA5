74c75a3c58d04d207b1e5544416a70e44ec4183c
wolfiee42 <saifalislam2022@gmail.com>
2023-08-21 09:49:44 +0600

    design and calculation part updated

--- script/utilities.js
+++ script/utilities.js
@@ -4,15 +4,7 @@ function namePrice(data) {
     const name = data.parentNode.childNodes[1].childNodes[5].innerText;
     const price = data.parentNode.childNodes[1].childNodes[7].innerText;
     const checkout = document.getElementById('checkOutSection').parentNode.childNodes[3].childNodes[1];
-    let discount = (total * (20 / 100)).toFixed(2);
 
-    document.getElementById('apply-btn').addEventListener('click', function () {
-
-        const discountAmmount = document.getElementById('discount');
-        discountAmmount.innerText = discount;
-        const finalAmmount = total - discount
-        document.getElementById('total').innerText = finalAmmount.toFixed(2);
-    });
 
     const p = document.createElement('p');
     p.innerText = name + ': ' + price;
@@ -30,12 +22,22 @@ function namePrice(data) {
 
     }
 
+    let discount = (total * (20 / 100)).toFixed(2);
+
+    document.getElementById('apply-btn').addEventListener('click', function () {
+
+        const discountAmmount = document.getElementById('discount');
+        discountAmmount.innerText = discount;
+        const finalAmmount = total - discount
+        document.getElementById('total').innerText = finalAmmount.toFixed(2);
+    });
+
     if (ammount >= 200) {
         document.getElementById('coupon-Code').addEventListener('keyup', function (event) {
             const input = event.target.value;
             const btn = document.getElementById('apply-btn');
 
-            if (input === "aa") {
+            if (input === "SELL200") {
                 btn.removeAttribute('disabled');
             } else {
                 btn.setAttribute("disabled", true)
